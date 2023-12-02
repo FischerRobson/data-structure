243a826935f33a437fd60b27c00845b515f5a27e
Robson Fischer <fischerrobson@gmail.com>
2023-12-02 17:39:41 -0300

    set and get methods in hashTables

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class HashTable {
     private int size = 7;
     private Node[] dataMap; // a array of pointers to linked lists
@@ -20,18 +23,64 @@ public class HashTable {
     public void printHashTable() {
         for (int i = 0; i < this.size; i++) {
             if (i != 0) System.out.println("---");
-            System.out.println("|" + i + "|");
+            System.out.print("|" + i + "| ");
 
+            Node temp = this.dataMap[i];
+            while (temp != null) {
+                System.out.print(" { " + temp.key + ": " + temp.value + " } ->");
+                temp = temp.next;
+            }
+            System.out.println();
         }
     }
 
     private int hash(String key) {
         int hash = 0;
         char[] keyChars = key.toCharArray();
-        System.out.println(keyChars);
         for (int asciiValue : keyChars) {
             hash = (hash + asciiValue * 23) % this.dataMap.length;
         }
         return hash;
+    }
+
+    public void set(String key, int value) {
+        int index = this.hash(key);
+
+        Node node = new Node(key, value);
+        if (this.dataMap[index] == null) {
+            this.dataMap[index] = node;
+        } else {
+            Node temp = this.dataMap[index];
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public int get(String key) {
+        int index = this.hash(key);
+
+        Node temp = this.dataMap[index];
+        while (temp != null) {
+            if (Objects.equals(temp.key, key)) {
+                return temp.value;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return 0;
+    }
+
+    public ArrayList<String> keys() {
+        ArrayList<String> keys = new ArrayList<>();
+        for (int i = 0; i < this.size; i++) {
+            Node temp = this.dataMap[i];
+            while (temp != null) {
+                keys.add(temp.key);
+                temp = temp.next;
+            }
+        }
+        return keys;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,22 @@ public class Main {
     public static void main(String[] args) {
         HashTable hashTable = new HashTable();
 
+        hashTable.set("Messi", 10);
+        hashTable.set("Ronaldo", 7);
+        hashTable.set("Sergio R.", 4);
+        hashTable.set("Neuer", 1);
+        hashTable.set("Khvicha", 77);
+        hashTable.set("Anguissa", 99);
+        hashTable.set("Harry Kane", 9);
+        hashTable.set("Goretzka", 8);
+        hashTable.set("Kimmich", 6);
+        hashTable.set("Davies", 19);
+        hashTable.set("Reus", 11);
+        hashTable.set("Lobotka", 68);
+        hashTable.set("Zielinsky", 20);
+
         hashTable.printHashTable();
+
+        System.out.println(hashTable.keys());
     }
 }
