b28bf7f57f82ee9fcb10d89c571f59bca8103ae3
Robson Fischer <fischerrobson@gmail.com>
2024-02-06 21:33:52 -0300

    recursive methods in binary search tree

--- src/Tree.java
+++ src/Tree.java
@@ -62,6 +62,70 @@ public class Tree {
         return false;
     }
 
+    public boolean recursiveContains(int value) {
+        return recursiveContains(this.root, value);
+    }
+
+    private boolean recursiveContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) {
+            return recursiveContains(currentNode.left, value);
+        } else {
+            return recursiveContains(currentNode.right, value);
+        }
+    }
+
+    public void recursiveInsert(int value) {
+        if (this.root == null) this.root = new Node(value);
+        recursiveInsert(this.root, value);
+    }
+
+    private Node recursiveInsert(Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+        if(value < currentNode.value) {
+            currentNode.left = recursiveInsert(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = recursiveInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    private int findSmallerValue(Node currentNode) {
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    public void deleteNode(int value) {
+        this.deleteNode(this.root, value);
+    }
+
+    private Node deleteNode(Node currentNode, int value) {
+        if (currentNode == null) return null;
+        if (value < currentNode.value) {
+            currentNode.left = deleteNode(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = deleteNode(currentNode.right, value);
+        } else {
+            // handle the value in the tree
+            if (currentNode.left == null && currentNode.right == null) {
+                return null;
+            } else if (currentNode.left == null) {
+                currentNode = currentNode.right;
+            }  else if (currentNode.right == null) {
+                currentNode = currentNode.left;
+            } else {
+                int subTreeMin = findSmallerValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                currentNode.right = deleteNode(currentNode, subTreeMin);
+            }
+        }
+        return currentNode;
+    }
+
     public int getHeight() {
         return this.getHeight(this.root) - 1;
     }
