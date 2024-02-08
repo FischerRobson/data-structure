2acc8e1edcbeb3dca70f9e21d7bcb8568e775371
Robson Fischer <fischerrobson@gmail.com>
2024-02-08 17:31:03 -0300

    tree traversal in order

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ public class Main {
     public static void main(String[] args) {
         Tree tree = new Tree();
 
-        tree.insert(6);
+        tree.insert(47);
         tree.insert(21);
-        tree.insert(18);
-        tree.insert(67);
-        tree.insert(12);
-        tree.insert(57);
-        tree.insert(99);
-        tree.insert(4);
-        tree.insert(36);
         tree.insert(77);
-        tree.insert(11);
-        tree.insert(5);
+        tree.insert(18);
+        tree.insert(27);
+        tree.insert(52);
+        tree.insert(82);
 
         tree.printTree();
+
+        System.out.println("Breadth-First Search: " + tree.BFS());
+        System.out.println("Deep First Search Pre Order: " + tree.DFSPreOrder());
+        System.out.println("Deep First Search Post Order: " + tree.DFSPostOrder());
+        System.out.println("Deep First Search In Order: " + tree.DFSInOrder());
     }
 }
